209ccfb06d872c1c57ef56e1fdd2d819cd6f9191
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-10-25 12:18:08 +0200

    Viderefører VERSION_TAG om den sendes inn i deploy-yaml

--- actions/pre-deploy/entrypoint.sh
+++ actions/pre-deploy/entrypoint.sh
@@ -17,7 +17,11 @@ while read -r id; do
 done < <(curl -s -H "Authorization: token $GITHUB_TOKEN" "$GITHUB_URL/actions/runs?event=push&status=in_progress&branch=master" | jq -r '.workflow_runs[].id')
 
 APPLICATION=$(echo $GITHUB_REPOSITORY | cut -d "/" -f 2)
-VERSION_TAG=$(TZ=Europe/Oslo date +"%y.%j.%H%M%S")
+if [ -z "$VERSION_TAG" ]; then
+  VERSION_TAG=$(TZ=Europe/Oslo date +"%y.%j.%H%M%S")
+else
+  VERSION_TAG=$VERSION_TAG
+fi
 if [ -z "$DOCKER_IMAGE" ]; then
   DOCKER_IMAGE=docker.pkg.github.com/$GITHUB_REPOSITORY/$APPLICATION
 fi
