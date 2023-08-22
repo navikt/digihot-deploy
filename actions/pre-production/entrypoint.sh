43b00bc18ac7244ab8a879bb1e15763635e2bf00
marte pedersen <marte.pedersen@nav.no>
2023-08-22 12:46:27 +0200

    deploy to prod workflow + pre-prod action

---
+++ actions/pre-production/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+DOCKER_REPO="europe-north1-docker.pkg.dev/$PROJECT_ID/$TEAM"
+
+if [ -z "$APPLICATION" ]; then
+  APPLICATION=$(echo $GITHUB_REPOSITORY | cut -d "/" -f 2)
+fi
+
+if [ -z "$DOCKER_IMAGE" ]; then
+  DOCKER_IMAGE=$DOCKER_REPO/$APPLICATION
+fi
+
+IMAGE=$DOCKER_IMAGE:$VERSION_TAG
+
+echo "IMAGE=$IMAGE" >> "$GITHUB_ENV"
